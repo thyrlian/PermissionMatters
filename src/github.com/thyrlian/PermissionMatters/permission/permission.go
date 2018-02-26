0cd9708ccfedb569503e938981c179582554991c
Jing Li <thyrlian@gmail.com>
2018-02-26 18:25:08 +0100

    Introduce Permission struct

---
+++ src/github.com/thyrlian/PermissionMatters/permission/permission.go
@@ -0,0 +1,24 @@
+package permission
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+type Permission struct {
+	Name string
+}
+
+func New(name string) Permission {
+	permission := Permission{strings.TrimSpace(name)}
+	return permission
+}
+
+func (p Permission) ToJson() string {
+	b, err := json.Marshal(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(b)
+}
