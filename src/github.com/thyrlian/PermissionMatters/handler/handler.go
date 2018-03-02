4bd5396096fbb0bde42e4a3a20b78da7a0697db6
Jing Li <thyrlian@gmail.com>
2018-03-02 07:23:51 +0100

    Print results

--- src/github.com/thyrlian/PermissionMatters/handler/handler.go
+++ src/github.com/thyrlian/PermissionMatters/handler/handler.go
@@ -7,22 +7,38 @@ import (
 )
 
 func Process(conclusion inspector.Conclusion) {
-	delimiter := "======================================================================"
+	linebreak := "======================================================================"
 	switch result := conclusion.Result; result {
 	case inspector.Pass:
-		fmt.Println(delimiter)
+		fmt.Println(linebreak)
 		fmt.Println("No permission is changed.")
-		fmt.Println(delimiter)
+		fmt.Println(linebreak)
 		os.Exit(0)
 	case inspector.Fail:
-		fmt.Println(delimiter)
-		fmt.Println("Fail")
-		fmt.Println(delimiter)
+		more := conclusion.More
+		less := conclusion.Less
+		fmt.Println(linebreak)
+		fmt.Println("Warning!")
+		fmt.Println(fmt.Sprintf("\n%d new permission(s) added:", len(more)))
+		for i := range more {
+			fmt.Println(fmt.Sprintf("    %s", more[i].Name))
+		}
+		if len(less) > 0 {
+			fmt.Println(fmt.Sprintf("\n%d old permission(s) removed:", len(less)))
+			for i := range less {
+				fmt.Println(fmt.Sprintf("    %s", less[i].Name))
+			}
+		}
+		fmt.Println(linebreak)
 		os.Exit(1)
 	case inspector.PassWithAttention:
-		fmt.Println(delimiter)
-		fmt.Println("Brilliant!  You got permissions removed:")
-		fmt.Println(delimiter)
+		less := conclusion.Less
+		fmt.Println(linebreak)
+		fmt.Println(fmt.Sprintf("Brilliant!  You got %d permission(s) removed:\n", len(less)))
+		for i := range less {
+			fmt.Println(fmt.Sprintf("    %s", less[i].Name))
+		}
+		fmt.Println(linebreak)
 		os.Exit(0)
 	}
 }
