7bd1dffad0361c692e12f00eb776f3eddc1f2988
Jing Li <thyrlian@gmail.com>
2018-03-03 12:55:50 +0100

    Add commander package

---
+++ src/github.com/thyrlian/PermissionMatters/commander/commander.go
@@ -0,0 +1,59 @@
+package commander
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const (
+	Take = iota
+	Scan = iota
+)
+
+type Order struct {
+	Operation int
+	Snapshot  string
+	Apk       string
+}
+
+func ParseArgs() Order {
+	var operation int
+	var snapshot string
+	var apk string
+	takeCmd := flag.NewFlagSet("take", flag.ExitOnError)
+	scanCmd := flag.NewFlagSet("scan", flag.ExitOnError)
+	takeSnapshotVal := takeCmd.String("snapshot", "./permissions.json", "Where to save the permission snapshot file.")
+	takeApkVal := takeCmd.String("apk", "", "The APK file to analyze.")
+	scanSnapshotVal := scanCmd.String("snapshot", "./permissions.json", "Where to save the permission snapshot file.")
+	scanApkVal := scanCmd.String("apk", "", "The APK file to analyze.")
+	switch os.Args[1] {
+	case "take":
+		operation = Take
+		takeCmd.Parse(os.Args[2:])
+	case "scan":
+		operation = Scan
+		scanCmd.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		fmt.Println("Require sub-command: 'take' or 'scan'")
+		os.Exit(2)
+	}
+	if takeCmd.Parsed() {
+		if *takeApkVal == "" {
+			takeCmd.PrintDefaults()
+			os.Exit(2)
+		}
+		snapshot = *takeSnapshotVal
+		apk = *takeApkVal
+	}
+	if scanCmd.Parsed() {
+		if *scanApkVal == "" {
+			scanCmd.PrintDefaults()
+			os.Exit(2)
+		}
+		snapshot = *scanSnapshotVal
+		apk = *scanApkVal
+	}
+	return Order{operation, snapshot, apk}
+}
